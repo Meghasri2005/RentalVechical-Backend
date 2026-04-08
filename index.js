39f396dd7813aafbaeb35d454efb691cab08fcdc
Meghasri2005 <chennamsettysiri2005@gmail.com>
2026-04-08 12:55:58 +0530

    adding link

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const app=express();
 require("dotenv").config()
 const PORT=process.env.PORT||4000;
 
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173", "http://localhost:3000", "https://rental-vechical-frontend-hucn.vercel.app"],
+    credentials: true,
+}));
 app.use(express.json());
 
 
